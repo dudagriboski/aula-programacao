c576082fc4ca38e58d272d6a0f3bcd7e69d6dfff
Maria Eduarda Griboski <eduardadasilvam0@gmail.com>
2025-07-02 08:42:24 -0300

    rr

--- aula_01-20250626T111145Z-1-001/aula_01/php/processar.php
+++ aula_01-20250626T111145Z-1-001/aula_01/php/processar.php
@@ -7,25 +7,12 @@ if($mysqli -> connect_error){
     exit();
 }
 
-if ($_SERVER['REQUEST_METHOD']=='POST'){
-    Somar($_POST['numero1'], $_POST['numero2']);
-    Multiplicar($_POST['numero1'], $_POST['numero2']);
-    Dividir($_POST['numero1'], $_POST['numero2']);
-    Subtrair($_POST['numero1'], $_POST['numero2']);
+if ($result = $mysqli -> query(@"INSERT INTO todo (idtodo, description) VALUES ((select max(idtodo)+1 from todo),
+                         'fazer ola mundo no php.');")){
+echo "Returned rows are: " . $result -> num_rows;
+$result -> free_result();
 }
 
-function Somar ($n1, $n2){
-    echo $n1 + $n2;
-}
+$mysqli -> close();
 
-function Multiplicar ($n1, $n2){
-    echo $n1 * $n2;
-}
-
-function Dividir ($n1, $n2){
-    echo $n1 / $n2;
-}
-
-function Subtrair ($n1, $n2){
-    echo $n1 - $n2;}
 ?>
